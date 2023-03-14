f5f408ebc0865734fef376b5158ee28ab5ae7efc
Mohammad Omar <mohammadomartaha17@gmail.com>
2023-03-14 16:51:12 +0200

    feat: sort app usage times

--- app/src/main/java/com/omar/mentalcompanion/services/UsageStatsService.kt
+++ app/src/main/java/com/omar/mentalcompanion/services/UsageStatsService.kt
@@ -5,13 +5,17 @@ import android.content.Context
 import android.content.Intent
 import android.os.Build
 
-data class AppUsage(val packageName: String, val usageTime: Long) {
+data class AppUsage(val packageName: String, val usageTime: Long) : Comparable<AppUsage> {
     override fun toString(): String {
         val hours = usageTime / 3600
         val minutes = (usageTime % 3600) / 60
         val seconds = usageTime % 60
 
         return "$packageName: $hours hours, $minutes minutes, $seconds seconds"
+    }
+
+    override fun compareTo(other: AppUsage): Int {
+        return other.usageTime.compareTo(this.usageTime)
     }
 }
 
@@ -66,7 +70,7 @@ class UsageStatsService(private val context: Context) {
             }
         }
 
-        return appUsages
+        return appUsages.sorted()
     }
 
     fun refresh() {
